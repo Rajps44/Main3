2327986d23471a71ec9d406b3644d80543974375
Rajps55 <hackinggm69@gmail.com>
2024-08-08 20:06:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False)) # True if you want n
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://rishiksingh234:JFBBXqm1lQhSCPC3@cluster0.xooxjcj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster1")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Verify
