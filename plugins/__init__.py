71cc48f7fcc726fdab22542acf74ef6d164b4ffa
Rajps44 <singhayush88805@gmail.com>
2024-08-17 22:39:53 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -3,6 +3,7 @@ from .route import routes
 
 
 async def web_server():
-    web_app = web.Application(client_max_size=30000000)
+    web_app = web.Application(client_max_size=4294967296)  # 4 GB in bytes
     web_app.add_routes(routes)
     return web_app
+
